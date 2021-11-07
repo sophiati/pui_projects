4628695702c3635d2c815aaab1f7e1bd9098525e
sophiati <sophiati@andrew.cmu.edu>
2021-11-07 18:17:31 -0500

    Cleaned up code

--- homework_6b/cart.js
+++ homework_6b/cart.js
@@ -1,31 +1,25 @@
-//Display cart as array of objects
-// const storedValue = JSON.parse(localStorage.getItem("savedCart"))
-// let cart
-// if (storedValue == null) {
-//   cart = []
-// } else {
-//   cart = storedValue
-// }
-// localStorage.clear()
-
+//On load, checks if items are in the cart or not
 window.onload = function () {
   const storedValue = JSON.parse(localStorage.getItem("savedCart"))
   console.log(storedValue)
+  //If there are no items in the cart, the page displays "There are no items in your cart!"
   if (storedValue === null) {
     document.getElementById("clear").style.display = "none"
     document.getElementById("no-items").style.display = "block"
+    //If the storedValue is not null, a "Clear Cart" button is shown below the cart items and the empty cart message is hidden
   } else if (storedValue !== null) {
     document.getElementById("no-items").style.display = "none"
     document.getElementById("clear").style.display = "block"
     updatePage()
-    // showIcon()
   }
 }
 
+//1 - Function to display the cart icon count, create clones for all objects in the cart, display those clones, create clones for all objects in the receipt, display the receipt clones, and calculate the receipt total
 function updatePage() {
+  //Displays amount of items in the cart in the icon in the top right
   document.getElementById("shopper").style.display = "block"
   document.getElementById("shopper").innerHTML = storedQty.toString()
-  //3 - Function to display products using templates & remove items from cart
+  //Uses a for loop to create a clone of the template for each item in the cart array
   const cartDiv = document.getElementById("items")
   const template = document.getElementById("cart-item-template")
   let i = 0
@@ -39,123 +33,86 @@ function updatePage() {
     clone.querySelector(".pieces-option").innerText = product.pieces
     clone.querySelector("#item_qty").value = product.quantity
     clone.querySelector(".price").innerText = "$" + product.total.toFixed(2)
+
+    //Sets a unique value for the id for each item in the array (to be used in the removeItem function as the index)
     product.id = i
+
     const button = clone.querySelector(".delete")
+    //Adds an event listener to the delete buttons within the clones
     button.addEventListener("click", function () {
       var cartItemsString = localStorage.getItem("savedCart")
       var cartQty = localStorage.getItem("cartQuantity")
       if (cartItemsString !== null) {
         var savedCart = JSON.parse(cartItemsString)
-        console.log(savedCart)
         var storedQty = JSON.parse(cartQty)
-        console.log("stored Qty: " + storedQty)
 
+        //Sets the index to the id value given to each item in the cart
         var ind = product.id
-        console.log(ind)
+
+        //Checks if the index is not the last one in the array and then splices into the array to remove the called item
         if (ind !== -1) {
           savedCart.splice(ind, 1)
           let itemQty = product.quantity
+          //Decrements the storedQty amount for the icon in the top right corner
           storedQty = storedQty - itemQty
-          console.log("storedQty 2: " + storedQty)
+          //Adjusts the localStorage for both the cart array and the counter array
           localStorage.setItem("savedCart", JSON.stringify(savedCart))
           localStorage.setItem("cartQuantity", JSON.stringify(storedQty))
+          //Reloads the page with the updates from the removal of the item
           window.location.reload()
         }
       }
     })
+
+    //Appends the clone to be displayed in the cartDiv
     cartDiv.appendChild(clone)
-    console.log(cartDiv)
   }
 
+  //Sets the cartDiv as visible
   cartDiv.style.visibility = "visible"
 
+  //For loop through the receiptDiv to clone and display each receipt item
   const receiptDiv = document.getElementById("receipt-item")
   const receiptTemplate = document.getElementById("receipt-item-template")
   let r = 0
   for (let r = 0; r < cart.length; r++) {
-    console.log(r)
     const product = cart[r]
     const clone = receiptTemplate.content.cloneNode(true)
     clone.querySelector(".item_name").innerText = product.name
     clone.querySelector(".multiplier").innerText =
       " (x" + product.quantity + ")"
     clone.querySelector(".cost").innerText = "$" + product.total.toFixed(2)
+    //Appends each clone to the receiptDiv
     receiptDiv.appendChild(clone)
   }
+
+  //Displays the receiptDiv
   receiptDiv.style.visibility = "visible"
 
+  //Calculates the subtotal of the items
   let subtotal = 0
   for (product of cart) {
     subtotal = subtotal + parseInt(product.total)
   }
+
+  //Displays the subtotal
   document.getElementById("subtotal").style.display = "block"
   document.getElementById("subtotal").innerHTML = "$" + subtotal.toFixed(2)
 
+  //Calculats the tax for the receipt
   const tax = subtotal * 0.07
+  //Calculates the grand total for the receipt (subtotal + tax)
   const grandTotal = subtotal + tax
-  console.log(grandTotal)
-  document.getElementById("subtotal").style.display = "block"
+
+  //Displays the subtotal, tax, and grand total
   document.getElementById("tax").innerHTML = "$" + tax.toFixed(2)
   document.getElementById("grand-total").innerHTML = "$" + grandTotal.toFixed(2)
 }
 
+//2 - Function to clear the cart
 function clearCart() {
-  console.log("clear")
+  //Clears all localStorage
   localStorage.clear()
+  //Reloads the page with nothing in the cart array or counter value
   window.location.reload()
-  // document.getElementById("clear").style.display = "hidden"
-  // document.getElementById("shopper").innerHTML =
-  //   "There are no items in your cart!"
 }
-
-// function removeItem() {
-//   var cartItemsString = localStorage.getItem("savedCart")
-//   if (cartItemsString !== null) {
-//     var savedCart = JSON.parse(cartItemsString)
-//     console.log(savedCart)
-
-//     var ind = savedCart.findIndex(function (item) {
-//       return item.id
-//       // item.name === self.name &&
-//       // item.glaze === self.glaze &&
-//       // item.quantity === self.quantity &&
-//       // item.pieces === self.pieces &&
-//       // item.total === self.total
-//     })
-
-//     console.log(item.id)
-
-//     // let ind = product.id
-//     // savedCart.splice(ind, 1)
-//     // localStorage.setItem("savedCart", JSON.stringify(cart))
-//     // console.log(savedCart)
-//     console.log("ind " + ind)
-//     if (ind !== -1) {
-//       savedCart.splice(ind, 1)
-//       localStorage.setItem("savedCart", JSON.stringify(cart))
-//       updatePage()
-//     }
-//   }
-// }
-
-// function removeItem(obj) {
-//   // retrieve the stored value of the cart items so that we can modify it
-//   var cartItemsString = localStorage.getItem("cartItems")
-//   if (cartItemsString !== null) {
-//     var cartItems = JSON.parse(cartItemsString) // successfully loaded in the cart items
-
-//     // find the index of the input object in the list
-//     var ind = cartItems.findIndex(function (item) {
-//       return item.glaze === obj.glaze && item.quantity === obj.quantity
-//     })
-//     console.log("ind " + ind)
-//     if (ind !== -1) {
-//       // remove item from the list
-//       cartItems.splice(ind, 1)
-//       // update the stored value
-//       localStorage.setItem("cartItems", JSON.stringify(cartItems))
-//       // re-render the page to reflect changes
-//       updatePage()
-//     }
-//   }
-// }
--- homework_6b/main.js
+++ homework_6b/main.js
@@ -1,6 +1,4 @@
-// localStorage.clear()
-
-//Display cart as array of objects
+//Check localStorage on each page to create an empty Cart array or a Cart array from localStorage savedCart
 const storedValue = JSON.parse(localStorage.getItem("savedCart"))
 let cart
 if (storedValue == null) {
@@ -20,19 +18,20 @@ function Item(itemName, itemGlaze, itemPieces, itemQty, itemTotal, itemImage) {
   this.id = 0
 }
 
+//2 - Wrapper function to show icon on the "cart" and add each item to the cart
 function clickToAdd() {
   showIcon()
   addToCart()
-  // updateReceipt()
 }
 
-//When item is added to cart, an alert shows with the quantity added and an icon displays and updates on the cart icon in the corner
+//3 - On page load, the cart icon displays the number of items in the cart
 let storedQty = JSON.parse(localStorage.getItem("cartQuantity"))
 window.onload = function () {
   document.getElementById("shopper").style.display = "block"
   document.getElementById("shopper").innerHTML = storedQty.toString()
 }
 
+//4 - Function to store and update cartCount and show the number of items in the cart in the corner
 function showIcon() {
   let storedQty = JSON.parse(localStorage.getItem("cartQuantity"))
   if (storedQty == null) {
@@ -48,11 +47,9 @@ function showIcon() {
   localStorage.setItem("cartQuantity", JSON.stringify(cartCount))
 }
 
-//2 - Function to add items to cart, display cart as array, and show numbers on cart
-const cartDiv = document.getElementById("items")
-
-function addToCart() {
-  var name = document.getElementById("item-name").innerText
+//5 - Updates price on product pages when different quantities and pieces per box are selected according to the price for each item
+function updatePrice() {
+  //Checks value of the pieces per box
   if (document.getElementById("one").checked) {
     var pieces = document.getElementById("one").value
   } else if (document.getElementById("three").checked) {
@@ -65,45 +62,107 @@ function addToCart() {
     var pieces = 1
   }
 
-  // Gets Image
+  //Checks value of the cost for each bun type
+  if (document.getElementById("item-name").innerHTML == "Original") {
+    var cost = 3
+  } else if (document.getElementById("item-name").innerHTML == "Walnut") {
+    var cost = 5
+  } else if (document.getElementById("item-name").innerHTML == "Blackberry") {
+    var cost = 4
+  } else if (
+    document.getElementById("item-name").innerHTML == "Caramel Pecan"
+  ) {
+    var cost = 5.5
+  } else if (document.getElementById("item-name").innerHTML == "Original GF") {
+    var cost = 4.5
+  } else if (
+    document.getElementById("item-name").innerHTML == "Pumpkin Spice"
+  ) {
+    var cost = 5.5
+  }
+
+  //Checks the quantity of boxes
+  var quantity = document.getElementById("qty").value
+
+  //Calculates the total cost for the item(s) being added to the cart
+  var total = cost * pieces * quantity
+
+  //Displays the updated price
+  document.getElementById("item_price").innerHTML = "$" + total.toFixed(2)
+}
+
+//6 - Function to add items to cart, display cart as array, and store the cart array in local storage
+const cartDiv = document.getElementById("items")
+
+function addToCart() {
+  //Creates name for the object being added
+  var name = document.getElementById("item-name").innerText
+
+  //Checks pieces per box
+  if (document.getElementById("one").checked) {
+    var pieces = document.getElementById("one").value
+  } else if (document.getElementById("three").checked) {
+    var pieces = document.getElementById("three").value
+  } else if (document.getElementById("six").checked) {
+    var pieces = document.getElementById("six").value
+  } else if (document.getElementById("twelve").checked) {
+    var pieces = document.getElementById("twelve").value
+  } else {
+    var pieces = 1
+  }
+
+  // Gets Image and cost for each bun type
   if (document.getElementById("item-name").innerHTML == "Original") {
     var image = "images/original.jpg"
+    var cost = 3
   } else if (document.getElementById("item-name").innerHTML == "Walnut") {
     var image = "images/walnut.jpg"
+    var cost = 5
   } else if (document.getElementById("item-name").innerHTML == "Blackberry") {
     var image = "images/blackberry.jpg"
+    var cost = 4
   } else if (
     document.getElementById("item-name").innerHTML == "Caramel Pecan"
   ) {
     var image = "images/caramel_pecan.jpg"
+    var cost = 5.5
   } else if (document.getElementById("item-name").innerHTML == "Original GF") {
     var image = "images/original_gf.jpg"
+    var cost = 4.5
   } else if (
     document.getElementById("item-name").innerHTML == "Pumpkin Spice"
   ) {
     var image = "images/pumpkin_spice.jpg"
+    var cost = 5.5
   }
 
+  //Checks glaze type selected for each object
   if (document.getElementById("none").checked) {
-    var glaze = "none"
+    var glaze = "None"
   } else if (document.getElementById("sugar_milk").checked) {
-    var glaze = "sugar milk"
+    var glaze = "Sugar Milk"
   } else if (document.getElementById("vanilla_milk").checked) {
-    var glaze = "vanilla milk"
-  } else if (document.getElementById("double_chocolate").checked) {
+    var glaze = "Vanilla Milk"
+  } else if (document.getElementById("double-chocolate").checked) {
     var glaze = "Double Chocolate"
   }
 
+  //Checks quantity for each object
   var quantity = document.getElementById("qty").value
-  var cost = 3
+
+  // var cost = 3
+  //Calculates total cost for the object item
   var total = cost * pieces * quantity
+
+  //Sets an id for the object (to be used for removeItem function as index)
   var id = 0
 
+  //Creates a new product with the attributes gathered from above
   const product = new Item(name, glaze, pieces, quantity, total, image, id)
-  console.log(product)
-  cart.push(product)
-  localStorage.setItem("savedCart", JSON.stringify(cart))
-  console.log(cart)
-}
 
-// console.log(cart)
+  //Adds the new object to the cart array
+  cart.push(product)
+
+  //Stores the cart array using localStorage
+  localStorage.setItem("savedCart", JSON.stringify(cart))
+}
