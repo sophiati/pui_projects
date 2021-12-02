e3ecc3067082d4f054457fc907c8f8b51dd89c8c
sophiati <sophiati@andrew.cmu.edu>
2021-12-02 12:58:27 -0500

    javascript updates

--- homework_8/main.js
+++ homework_8/main.js
@@ -7,71 +7,100 @@ if (storedValue == null) {
   plan = storedValue
 }
 
+function startQuiz() {
+  window.location.href = "fake_quiz.html"
+  clearQuiz()
+}
+
+function clearQuiz() {
+  localStorage.clear()
+}
+
 //1 - Function to create objects for each item
 function Item(
   itemQuestion,
   itemGoal,
   itemABC,
   itemWW,
-  itemMethod1,
-  itemMethod2,
-  itemMethod3
+  itemMethods
+  // itemMethod1,
+  // itemMethod2,
+  // itemMethod3
 ) {
   this.question = itemQuestion
   this.goal = itemGoal
   this.abc = itemABC
   this.ww = itemWW
-  this.method1 = itemMethod1
-  this.method2 = itemMethod2
-  this.method3 = itemMethod3
+  this.methods = itemMethods
+  // this.method1 = itemMethod1
+  // this.method2 = itemMethod2
+  // this.method3 = itemMethod3
 }
 
-function submit() {
+function submitQuiz() {
   var question = document.getElementById("question").value
 
   //Checks Goal
   if (document.getElementById("strategize").checked) {
-    var goal = document.getElementById("strategize").value
+    var goal = "Strategize"
   } else if (document.getElementById("execute").checked) {
-    var goal = document.getElementById("execute").value
+    var goal = "Execute"
   } else if (document.getElementById("assess").checked) {
-    var goal = document.getElementById("assess").value
+    var goal = "Assess"
   }
+
+  console.log(goal)
 
   //Checks Focus - AB
   if (document.getElementById("attitudes").checked) {
-    var abc = document.getElementById("attitudes").value
+    var abc = "attitudes"
   } else if (document.getElementById("behaviors").checked) {
-    var abc = document.getElementById("behaviors").value
+    var abc = "behaviors"
   }
+
+  console.log(abc)
 
   //Checks Focus - WW
   if (document.getElementById("what").checked) {
-    var ww = document.getElementById("what").value
+    var ww = "what"
   } else if (document.getElementById("why").checked) {
-    var ww = document.getElementById("why").value
+    var ww = "why"
   }
+
+  console.log(ww)
 
   //Selects Methods
   if (abc == "behaviors" && ww == "what") {
-    var method1 = document.getElementById("ab-testing")
-    var method2 = document.getElementById("heuristic")
-    var method3 = document.getElementById("web-analytics")
+    var methods = 1
+    // var method1 = document.querySelector("#ab-testing")
+    // var method2 = document.querySelector("#heuristic")
+    // var method3 = document.querySelector("#web-analytics")
   } else if (abc == "behaviors" && ww == "why") {
-    var method1 = document.getElementById("usability-testing")
-    var method2 = document.getElementById("think-aloud")
-    var method3 = document.getElementById("contextual-inquiry")
+    var methods = 2
+    // var method1 = document.querySelector("#usability-testing")
+    // var method2 = document.querySelector("#think-aloud")
+    // var method3 = document.querySelector("#contextual-inquiry")
   } else if (abc == "attitudes" && ww == "why") {
-    var method1 = document.getElementById("diaries")
-    var method2 = document.getElementById("codesign")
-    var method3 = document.getElementById("interviews")
+    var methods = 3
+    // var method1 = document.querySelector("#diaries")
+    // var method2 = document.querySelector("#codesign")
+    // var method3 = document.querySelector("#interviews")
   } else if (abc == "attitudes" && ww == "what") {
-    var method1 = document.getElementById("speed-dating")
-    var method2 = document.getElementById("card-sorting")
-    var method3 = document.getElementById("survey")
+    var methods = 4
+    // var method1 = document.querySelector("#speed-dating")
+    // var method2 = document.querySelector("#card-sorting")
+    // var method3 = document.querySelector("#survey")
   }
+  console.log(methods)
 
-  const research = new Item(question, goal, abc, ww, method1, method2, method3)
+  const research = new Item(question, goal, abc, ww, methods)
+  console.log(research)
+
+  plan.push(research)
+
+  localStorage.setItem("savedPlan", JSON.stringify(plan))
+
+  window.location.href = "fake_results.html"
 }
 
 // //Open Modals
